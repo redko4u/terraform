3acfe5f707167cd6a3565a503f2431a023019b09
redko4u <kondavenkat537@gmail.com>
2025-08-21 16:48:19 +0530

    terraform

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,7 +1,7 @@
 # resource <resource-type> <resource-name>
 resource "aws_instance" "db" {
 
-    ami = "ami-090252cbe067a9e58"
+    ami = "ami-09c813fb71547fc4f"
     vpc_security_group_ids = [aws_security_group.allow_ssh.id]
     instance_type = "t3.micro"
 
@@ -31,6 +31,6 @@ resource "aws_security_group" "allow_ssh" {
 
     tags = {
         Name = "allow_ssh"
-        CreatedBy = "Sivakumar"
+        CreatedBy = "KONDA"
     }
 }
