32f3fbb0f5c4febc72d112cd0bc15423c26033b0
redko4u <kondavenkat537@gmail.com>
2025-08-21 16:27:25 +0530

    terraform

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -1,34 +1,34 @@
 # resource <resource-type> <resource-name>
 resource "aws_instance" "db" {
 
-    ami = var.image-id
-    vpc_security_group_ids =  [aws_security_group.allow_ssh.id]
+    ami = var.image_id
+    vpc_security_group_ids = [aws_security_group.allow_ssh.id]
     instance_type = var.instance_type
-    
+    # left side things are called as arguements, right side are values.
     tags = var.tags
 }
 
 resource "aws_security_group" "allow_ssh" {
-    name = "var.aws_security_group"
-    description = "var.sg_description"
+    name = var.sg_name
+    description = var.sg_description
 
-    #terraform block
+    # this is block
     ingress {
-        from_port        = 22
-        to_port          = 22
-        protocol         = "tcp"
-        cidr_blocks      = ["0.0.0.0/0"]
+        from_port        = var.ssh_port
+        to_port          = var.ssh_port
+        protocol         = var.protocol
+        cidr_blocks      = var.allowed_cidr
     }
 
     egress {
         from_port        = 0 # from 0 to 0 means, opening all protocols
         to_port          = 0
         protocol         = "-1" # -1 all protocols
-        cidr_blocks      = ["0.0.0.0/0"]
+        cidr_blocks      = var.allowed_cidr
     }
 
     tags = {
-        Name = "allow-all_ssh"
+        Name = "allow_ssh"
         CreatedBy = "Konda"
     }
 }
--- variables/variables.tf
+++ variables/variables.tf
@@ -1,21 +1,16 @@
-variable "image-id" {
-    type = string
-    default = "ami-090252cbe067a9e58"
-    description = "RHEL-9 AMI ID"
+#1. command line
+#2. tfvars
+#3. ENV variable
+#4. variable default value
+variable "image_id" {
+  type        = string #optional
+  default     = "ami-090252cbe067a9e58" #optional
+  description = "RHEL-9 AMI ID" #optional
 }
 
 variable "instance_type" {
     default = "t3.micro"
     type = string
-}
-
-variable "aws_security_group" {
-    default = "Practice-Security-Group"
-    type = string
-}
-
-variable "aws_security_group_id" {
-    default = "sg-073530d8f67d1b474"
 }
 
 variable "tags" {
@@ -26,10 +21,24 @@ variable "tags" {
         Name = "DB"
     }
 }
-variable "ssh_port" {
-    default = 22
+
+variable "sg_name" {
+    default = "allow_ssh"
 }
 
 variable "sg_description" {
     default = "allowing port 22"
+}
+
+variable "ssh_port" {
+    default = 22
+}
+
+variable "protocol" {
+    default = "tcp"
+}
+
+variable "allowed_cidr" {
+    type = list(string)
+    default = ["0.0.0.0/0"]
 }
